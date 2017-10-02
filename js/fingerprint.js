804f515c530e97ae21d5fa688007b2325d97a1d4
Rom Valme <rom.valme@gmail.com>
2017-10-02 02:38:19 -0400

    making more changes removing files

--- js/fingerprint.js
+++
@@ -1,6 +0,0 @@
-
-function userTrack() {
-        var x = "User-agent header sent: " + navigator.userAgent;
-        document.getElementById("user_agent").innerHTML = x;
-}
-window.onload = userTrack
